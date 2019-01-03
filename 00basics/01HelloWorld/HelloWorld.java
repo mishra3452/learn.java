83e3f82556caff2f7f2f25c039c379ba2ca1224f
mishra3452 <mishrasumit.3452@gmail.com>
2019-01-03 19:08:42 +0530

    added HelloWorld.java

---
+++ 00basics/01HelloWorld/HelloWorld.java
@@ -0,0 +1,21 @@
+/*
+Here we will learn to print the traditional "Hello World!" using java.
+Kindly see the description.md  file to have a closer look at the below program.
+*/
+/*
+Comments in java are written same as that in c++.
+Single line comment is given by //(some comment)
+Multiline comment can be done in the same way as used in the description above
+*/
+class HelloWorld{
+    public static void main( String a[]){
+        System.out.println("Hello World!");
+    }
+}
+
+/* 
+Output :
+
+Hello World!
+
+*/
