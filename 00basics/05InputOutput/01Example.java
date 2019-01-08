33e5feb64b8f76b11e95edaca64eed74c56c2029
mishra3452 <mishrasumit.3452@gmail.com>
2019-01-08 18:03:15 +0530

    Input Output In Java

---
+++ 00basics/05InputOutput/01Example.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class Example{
+    public static void main(String a[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter any number");
+        int x = sc.nextInt();
+        System.out.println("Enter other number");
+        int y = sc.nextInt();
+        System.out.println(x+y);
+    }
+}
